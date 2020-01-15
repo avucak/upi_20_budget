6321e68f5faf479a87cd0d57c435cb03e6f4cde1
Lucija <lucija.brocic@gmail.com>
2020-01-15 10:30:43 +0100

    ispravak modela transakcije i preimenovanje

--- projekt_budget/budget/model_transaction.py
+++ projekt_budget/budget/model_transaction.py
@@ -16,7 +16,7 @@ class TransactionModelSQLite:
     def __init__(self, filename):
         self.conn = sqlite3.Connection(filename)
         self.cur = self.conn.cursor()
-
+        self.cur.execute("PRAGMA foreign_keys = ON")      
         self.cur.executescript("""
             CREATE TABLE IF NOT EXISTS transactions(
               id integer PRIMARY KEY,
@@ -25,9 +25,7 @@ class TransactionModelSQLite:
               amount real NOT NULL,
               date Date NOT NULL,
               note string,
-              CONSTRAINT fk_category
-              FOREIGN KEY (category)
-              REFERENCES categories (id));""")
+              FOREIGN KEY(category) REFERENCES categories(id));""")
 
     def transaction_select(self, transactionId=None, name=None, category=None, amount=None, date=None):
         sql = "SELECT * FROM transactions "
@@ -97,28 +95,5 @@ class Transaction:
         return "Transaction(" +self._name+", "+ self._category+", "+str(self._amount) +", "+self._date+")"
 
 
-import unittest
-
-class TestTransactionModel(unittest.TestCase):
-    def setUp(self):
-        self.db = TransactionModelSQLite("unit_database.db")
-        self.dbCat = model_category.CategoryModelSQLite("unit_database.db")
-
-    def tearDown(self):
-        del(self.db)
-        del(self.dbCat)
-        os.remove("unit_database.db")
-
-    def test_transaction_2_inserts(self):
-        self.dbCat.category_insert("Bills")
-        self.dbCat.category_insert("Rent")
-        self.dbCat.category_insert("Food")
- 
-        self.db.transaction_insert("Water bill",1, 150, "2019-12-05")
-        self.db.transaction_insert("Dinner",3, 79.99, "2019-12-10", "moja napomena")
-        self.assertCountEqual(self.db.transaction_select(), [(1,"Water bill","Bills", 150, "2019-12-05",""), (2, "Dinner",3, 79.99, "2019-12-10", "moja napomena")])
-
-if __name__ == "__main__":
-    unittest.main()
 
     
