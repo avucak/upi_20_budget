f360ae9364335c64e17d161d02151f821acf1078
Lucija <lucija.brocic@gmail.com>
2020-01-20 19:15:29 +0100

    implementacija sort-a

--- projekt_budget/budget/view.py
+++ projekt_budget/budget/view.py
@@ -13,8 +13,8 @@ class CategoryView():
 class TransactionView():
 
     @staticmethod
-    def transactionShow(categories=[], transactions=[], catChecked=[], minA="", maxA="", minD="", maxD=""):
-        return template('transactions', categories=categories, transactions=transactions, categoriesChecked=catChecked, minAmount=minA, maxAmount=maxA, minDate=minD, maxDate=maxD)
+    def transactionShow(categories=[], transactions=[], catChecked=[], minA="", maxA="", minD="", maxD="", option="other"):
+        return template('transactions', categories=categories, transactions=transactions, categoriesChecked=catChecked, minAmount=minA, maxAmount=maxA, minDate=minD, maxDate=maxD, option=option)
     
     @staticmethod
     def transactionAdd(categories=[], disable="false"):
